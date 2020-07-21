1c34e4fcfd3ba841c54f10e0c78c77dacdec7591
visiontek-sample-client <visiontek-sample-client@visiontek.co.in>
2020-07-21 12:08:49 +0530

    App versions file added

--- src/get_app_info.c
+++ src/get_app_info.c
@@ -1,94 +1,90 @@
-#include <header.h>
-
-int  get_app_details()
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<malloc.h>
+int App_updates();
+int main()
 {
+	App_updates();	
+	return 0;
+}
 
-	FILE *fp;
+int App_updates()
+{
+	FILE *fp = NULL;
 
-	int i=0,j=0,k=0;
-	char buff[256];
-	char A_type[50]="";
-	char A_ver[50]="";
+	char *line=NULL,*str=NULL;
+	int Update_count,Update_flag;
+	size_t len=20;
+	int i,j,check=0,  Total_Device_Apps=0,  Total_Server_Apps=0;
 
-	fp = fopen("/mnt/sysuser/app_ver","r");
+	char DeviceApplicationType[50][128];
+	char DeviceApplicationName[50][128];
+	float DeviceApplicationVersion[50];
 
-	if (fp==NULL)
+
+	memset(DeviceApplicationType,0,sizeof(DeviceApplicationType));
+	memset(DeviceApplicationName,0,sizeof(DeviceApplicationName));
+	memset(DeviceApplicationVersion,0,sizeof(DeviceApplicationVersion));
+
+	fp = fopen("/etc/visiontek_Application_release","r");
+	if(fp == NULL)
 	{
-		strcpy(module.app_type,"ERROR");
-		strcpy(module.app_version,"ERROR");
-		fprintf(stderr,"Application Version File open error\n");
+		fprintf(stdout," /etc/visiontek_Application_release  file not found \n");
 		return -1;
 	}
 
-	for (i=0;i<4;i++)
+	for(i=0,j=0;getline(&line, &len, fp) > 0;)
 	{
-		memset(buff,0x00,256);
-		fscanf(fp, "%s",buff); 
-
-
-		if (i==0)
-		{	
-			for(j=0;j<100;j++)
-			{
-				if (buff[j]==':')
-				{	
-					for (k=0;k<(strlen(buff)-j);k++)	
-						A_type[k]=buff[j+k+1];
-					fprintf (stdout, "Application Type:%s\n",A_type);
-					break;
-				}
-
-
-
-
-			}
-
-
-
-		}
-
-
-		if (i==1)
+		if((str = (char *)strstr(line,"ApplicationType:")) != NULL)
 		{
-			for(j=0;j<100;j++)
-			{
-				if (buff[j]==':')
-				{
-					for (k=0;k<(strlen(buff)-j);k++)
-
-						A_ver[k]=buff[j+k+1];
-
-					fprintf (stdout, "Application Version:%s\n",A_ver);
-					break;
-				}
-
-
-
-
-			}
-
+			strcpy(DeviceApplicationType[i],str+16);
+			if(DeviceApplicationType[i][ strlen(DeviceApplicationType[i]) -1 ] == '\n')
+				DeviceApplicationType[i][ strlen(DeviceApplicationType[i]) - 1 ]='\0';
+			i++;
 		}
+		else if((str = (char *)strstr(line,"ApplicationName:")) != NULL)
+		{
+			strcpy(DeviceApplicationName[j],str+16);
+			if(DeviceApplicationName[j][strlen(DeviceApplicationName[j])-1] == '\n')
+				DeviceApplicationName[j][strlen(DeviceApplicationName[j])-1]='\0';
+			j++;
+		}
+		else if((str = (char *)strstr(line,"Version:")) != NULL)
+		{
+			if( i > 0 && i == j )
+				DeviceApplicationVersion[j-1]  = atof(str+8);
+
+			else 
+			{	
+				fprintf(stdout," %d  %d \n", i,j);
+				fprintf(stdout,"Wrong Application Response in /etc/visiontek_Application_release\n");
+				return 0;
+			}
+		}
+
+
+		else fprintf(stdout,"Line = %s \n",line);
+
+		if ( i == 49  )
+		{
+			fprintf(stdout,"Caution: Maximum 50 applications are servered, skipping remains");
+			break;
+		}
+
+
 	}
+	Total_Device_Apps = i;
+	fprintf(stdout," Total_Device_Apps = %d\n", Total_Device_Apps);
+	fprintf(stdout," %d  %d \n", i,j);
+	free(line);
+	line=NULL;
+	fclose(fp);
 
+	for(i=0;i<Total_Device_Apps;i++)
 
-	memset(module.app_type,0,sizeof(module.app_type));
+		fprintf(stdout,"ApplicationType =%s ,ApplicationName=%s,ApplicationVersion= %.1f \n",DeviceApplicationType[i],DeviceApplicationName[i],DeviceApplicationVersion[i]);
 
-	if ( strlen(A_type) != 0 )	
-	strcpy(module.app_type,A_type);
-	else 
-		strcpy(module.app_type,"ERROR");
-
-
-	memset(module.app_version,0,sizeof(module.app_version));
-
-	if ( strlen(A_ver) != 0 )	
-	strcpy(module.app_version,A_ver);
-	else 
-		strcpy(module.app_version,"ERROR");
 
 	return 0;
-
-
-
 }
-
