30bad031ff5fa99c5699ccccd5c5aa4ce8ccc895
nareshmaramaina <naresh.m@visontek.co.in>
2020-07-22 15:51:00 +0530

    ADded

---
+++ src/Update_Configured_Server_Addr.c
@@ -0,0 +1,42 @@
+#include <header.h>
+char Server_Addr[128]="https://rhms2.callippus.co.uk";
+int  Update_Configured_Server_Addr(char *Server_Addr)
+{
+	FILE *fp;
+	size_t len=0;
+	char *str=NULL; 
+
+
+	fp = fopen("/etc/.RHMS_Server.config","r");
+
+	if (fp == NULL)
+	{
+		fprintf(stderr," /etc/.RHMS_Server.config file not found, Proceeding with default address = %s\n",Server_Addr);
+		return -1;
+	}
+
+	memset(Server_Addr,0,sizeof(Server_Addr));
+	while((getline(&str,&len,fp)) != -1)
+	{
+		if  (strstr(str,"ServerAddress:") != NULL )
+		{
+			fprintf(stdout," Updated Configured Server Address\n");
+			strcpy(Server_Addr,str+14);
+			break;
+		}
+	}
+	free(str);
+	str=NULL;	
+	fclose(fp);
+
+	if(Server_Addr[strlen(Server_Addr)-1] == '\n')
+		Server_Addr[strlen(Server_Addr)-1]='\0';
+
+	if ( strlen(Server_Addr) < 12 )
+	{
+		fprintf(stdout,"Invalid Configured server Address in  /etc/.RHMS_Server.config\n");
+		strcpy(Server_Addr,"https://rhms2.callippus.co.uk");
+	}
+	fprintf(stdout," Server Address is %s\n", Server_Addr);
+	return 0;
+}
